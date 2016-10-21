e1200fa7fad49f4df1f5ec59894baa04462f9264
kwall2004 <kwall2004@gmail.com>
2016-10-21 14:17:01 -0400

    new

--- index.js
+++ index.js
@@ -1,47 +1,129 @@
 var request = require('request');
+var elasticsearch = require('elasticsearch');
 
-var token = 'KdOGWvMrRnrK6MKT6K6Fr_SJcZpybTiZ4NQCY-Wl4fwwRMWnHdoUh5S4jGLu-IPgdpr_c-IbeIyjb2ioU4uRMZL4aIRn0lXkUKPjQ9AjNwKgBwbtuOO3C2NhH8Jb7QAWQy0VBjBJPuofGlHWv-7QgDkLA3Ajulzw_4K5ygo5vkOYzX1xZ7rGcmItrSgycA6nVLVaI_8K3_OdfqymFug1Ln7H2kqfVnAdCVpsMaUiFCapxZ_2vF-QbXcfaUELHCuJge432tXxeVSXYFczQXN-opllZXFZXoSd9AYtSgq_k8z1mBpwa8vyuE4kNV5z0gIhWIYS1gYqxcbPk5RBdTtBGFYTTLtw4kfrrL5JHTcHATlVlK4kZ7Ui_K7HfF3uCFSq6ScB3_VOp-8fNA5g6GYM_neVi4iZiI32auVEgl0SL1RpHKHTjgXSlhmDyOgcP84MDYEdivaA9zSFBir8iMdC8Ijc_FjFr5FS5FwMB7AJZtezZYENqOxU2igN8jiFqLtzr0ACUuQz6zDl-LIrpZUQxQYbB_6WB9ozynY_9j5f99Xcrs9ai4V5KyIePHQ5wsTJCIflMh1gXh1IuzTY_ttQiv0KGd53oNW7aHoX78hYh7ASndgTdbIKOLE7DZuq2egf-dEsDK3OwCjaRSB3Xy173yyN6xGqRa2XnIEqT46cYKxKd1lI6ZEoWjwC90XSqYZ-ksppmTPxoYnsZLkMSBOjOpINgLtHr7L5hQE1st_EaQfAWjX6LENM36WFgdyfwFUNxvGht2e4y5YBWEeWXroRQioAjtdI5ABC7gb5cmyBYxMgkM4xbzfl5IMhC2TmFLQOdbEVTPAM9lEDFhNp_h101HPWV75p_JUWB5nnfQTsKJQUrSeIDtd8iX0ousf5KrxxNhcp8PF-gT-vcFWaiqBVbCU9GliMCQPDm_6yGHiHDlYaJgUI1n_4Ku4LWmrywUjy0ttUifaRfsfqKpcp7TvvhzqhBR2izWuT4o5BXHhpBT5p6198S5d4Mw81nLEo04tsybVaSe9FaLAG18GkdM9JW3BYZDz87qsjX4AnlLMECIShtbkdYNtYGKdXgYm5UrN45SzmNRFlmXo3l8a-5YNSmh8RlvLNe9WOXyyVK5t_jbfDMHdKYj7oCuBUO18Z7Nt0K96euvd1I86QRncPofmi79ZStcTRpLu1EzlwvoL5HJ0tW-ieiHRijXBR1VEjggjup_KZhKQGBbAFi3Hg3kzZN-dx0ZIzXxAVJM_a9o35qinbXhCkK7yvFk0qs1jSPuZxaNt5zZ6Yg8d5QP8R5B-nG6JKFPijauBXR5A8iTNpmv1MyHdvA1V7N0Q05i-YQWFuNurLDQQ15tkkx064wKFHu3iHAls3FLZEaYaqvmnIYKN05RpPkxrtW8x_9ESOQUGI5x9NPzAQgs2AEbsKmn7m7W49XZTbqSAiunZkZxENUrSG17wSas0Jlpsv345vgiMNletodgPHHG75ZrZTC3QOupcesRh89Y4hSAgPysyw1kskQvi_Gvg0CFGlQzXut8_49n2P2-udCe5XOeCZ0z7McoWCjCnNWzMn_7vhfCWqVWZ-E_408EaCrRaqyBgY5_Ec';
+var token = 'rujhU_ZeTFgC8nnrGS3fJQ9SnmbmNRSDKd6WrSLdKljb6HMd_4hBeRGItvC68NDfyuPvSmOILBeGSJtDrMdr27ZnSTnDBOKFSsR9xftz5RMr_DOH6ZBEXa5Xm61LXGGx01T2rXTIZSOc2_hMMMAwJ1JMbF_yJjHqdCsQ4dAiLHj6rhOBwhtpE4gh3-3MZ9udnWu9eHXzvn59vX015hqsymqCfez3tdBueLUz5O9FRB-STGWQrlHQhq7naXLth9DZvbjvtkPV4Wv-KRoRyI1WQwWXBNeOUxYK7loght_zrMra3DWwzJlLe3aAnrcHzq0cFHpyWfWdmPZUmMWq6RLZc1alPVm1O3khISOxq6Xswdb2tbvKcG-eZ3Y20b53NltZcJd-GwkeIcL11j8IAYpk2CHXdESFlXlAqwLeQnZyVuhU-B2108RcnCwAAb4xnJKhY_Io13EbQ1bwTbwdy4AoujEIHBHjMKuEJQbf4GCfy3U_AqABXZ1ouA0r4D-a-FUA4mmuHCVvaSX5Z7ZAQcft7ENrY8bU1jjqQLe2WXjiD9DxLae9dUtsagBH2aCfGK9cBW6qe_6Kw7C_3X7Hex4m0BGf8HpU7Ttw1I7u6IAo0aIJiRo-CwzOeOMstyLKLLapNsi0WZvfEZq2qCmgfXDUlKiSX0mwEcGXCBNfQXR5AG__df4eHHtGQp6vBHkB9oRnEQiJESoUxuXbW3gSdRBZPEGIU0iCKSfYuW1nCJzkIFXoPKP6AVU_ArrOGibMvRkTOgOz9aGBVBON2JpjmgWReKziKwgR5ni5lvYJTX7l4n6n38sX7UWGPxARTgyhQgHIPFp7j-QZlYLFm55QmGzR03l84Au8zkgMPtlNOq1Jq3jMva9FrlHeD5ARaoiGyfx3V1ui3213xp8xYnYqaWgt7udJ4jVNkSzlNI4BH2dYscYLxxtlJ9wpCL1januKJtXVyN7bJVM8vX5ZGYZfuE3CXjFxWwmviuPRg6f8kMDMEPXGIsW1xBS5hVmiUciY7C7F2eYTwLhaH4rOHgdNLjOcG-9pK1PDJIlHKh1t8Icv2ewuLfTRi8awnx-Ne0A2318tVWvGI21me-FmYUXjpSvBgWIy5u6Uhq7sba_BJHxjaFWssEHiYx3hubBjMgLUC_1ZP4i-Vk4soDzn32yBpaDtbfwnw3S7WiBkIk0b4s998pFMH_XFqPW2-OPafM8z3PLpOoAecEZDNN-qt0aKSmBlAI3DEw-ZiTmpyLarTuw8lUfNFgprZe_CGdiOnV_PbvJifx8sM9PPuIz5f9kJSpctjY_JvuIAw7sMpD4z1asMRNZ6maQdfYsNGb6s1L_sRv6P8e2k5jjGM5RQuCtQQI8JqBeA0pDvPhKgDMjLoH9FhkMgGqjTUppSlmHLdUWqigBYwwuqSaiHu425XiG3A0NawdCGP1BcJTMqtUBlbiVAxMANpkqNb73DSizXzExbK3j_tc6p4xGqyPr7gHhprBcr1Oa5RMRlY00p3eLqiurNZ8eceM16XqZ6v1S3ip7JjhNjfLCZxfXO9rPjRkRRAqUoXGHcIewVWgQOylr9JpYvdRiuBA5ElEYs1vFRE5Za9sjL';
 
-request.get(
+var client = new elasticsearch.Client();
+client.indices.exists(
   {
-    url: 'http://localhost:65027/api/Trips/GetTrips?VehicleId=105342&SelectedDate=2016-05-20',
-    auth: {
-      bearer: token
-    }
+    index: 'vision'
   },
-  function (error, response, body) {
+  function (error, response, status) {
     if (error) {
-      console.log(error);
-      return;
+      throw new Error();
     }
 
-    if (response.statusCode == 200) {
-      var data = JSON.parse(body);
-      data.forEach(function (element) {
-        console.log(element.trip.tripId);
-        request.get(
-          {
-            url: 'http://localhost:65027/api/Trips/GetTripJsonData?TripId=' + element.trip.tripId,
-            auth: {
-              bearer: token
-            }
-          },
-          function (error, response, body) {
-            if (error) {
-              console.log(error);
-              return;
-            }
-
-            if (response.statusCode == 200) {
-              var data = JSON.parse(body);
-              console.log(data[0]);
+    if (!response) {
+      client.indices.create(
+        {
+          index: 'vision',
+          body: {
+            mappings: {
+              trip: {
+                properties: {
+                  data: {
+                    properties: {
+                      m12: {
+                        type: 'double'
+                      },
+                      m13: {
+                        type: 'double'
+                      },
+                      x: {
+                        type: 'double'
+                      },
+                      y: {
+                        type: 'double'
+                      },
+                      z: {
+                        type: 'double'
+                      },
+                      l: {
+                        type: 'double'
+                      },
+                      n: {
+                        type: 'double'
+                      }
+                    }
+                  }
+                }
+              }
             }
           }
-        )
-      });
+        },
+        function (error) {
+          if (error) {
+            throw new Error();
+          }
+
+          migrate();
+        }
+      );
     }
     else {
-      console.log(response);
+      migrate();
     }
   }
 );
+
+function migrate() {
+  request.get(
+    {
+      url: 'http://localhost:65027/api/Trips/GetTrips?VehicleId=105342&SelectedDate=2016-05-20',
+      auth: {
+        bearer: token
+      }
+    },
+    function (error, response, body) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      if (response.statusCode == 200) {
+        var data = JSON.parse(body);
+        data.forEach(function (element) {
+          console.log(element.trip.tripId);
+          request.get(
+            {
+              url: 'http://localhost:65027/api/Trips/GetTripJsonData?TripId=' + element.trip.tripId,
+              auth: {
+                bearer: token
+              }
+            },
+            function (error, response, body) {
+              if (error) {
+                console.log(error);
+                return;
+              }
+
+              if (response.statusCode == 200) {
+                var data = JSON.parse(body);
+                var body = Object.assign({}, element.trip, { data: data });
+                console.log(body);
+                // client.index(
+                //   {
+                //     index: 'vision',
+                //     type: 'trip',
+                //     body: body
+                //   },
+                //   function (error, response) {
+                //     if (error) {
+                //       console.error(error.response);
+                //       throw new Error();
+                //     }
+                //
+                //     console.log(response);
+                //   }
+                // );
+              }
+            }
+          );
+        });
+      }
+      else {
+        console.log(response);
+      }
+    }
+  );
+}
