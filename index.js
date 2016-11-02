3ebd5c42fbc545e073a2217e85889986fb785983
kwall2004 <kwall2004@gmail.com>
2016-11-02 16:19:40 -0400

    latest

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var request = require('request');
 var elasticsearch = require('elasticsearch');
 
-var token = '8nOtwI7LKKMk-WYdOnDZylrVQNiBc2-BJGUk0NoTwPlWqhXVA2Z0pyNctYZikcnP_CEZIXMGCgjXDqUABInxYkSbwuKg3D7fzcx3pBVJTaMq6lnxD1zQPBx-z6YLM0La_wuxnt9Ggzhm-GvkXhiR1wUZuVbTmw7Z7-UI9RI8nxftPRML55nDWhSjFt-mnmSb2WC9r5h5Dbj1tjqVFIXrVE4hc9CvxsoHSmA62fjHwnMrvU7HaBWk2sKIoykzT0hGfizUPiwOjZANoFLgxJ8GN_QVmgAjZRE903Ai5pTUUTzDb009wf-A4qlFhYWCEi9_9xULH2FBIEAlSY6j61I_IdftU0ZE_f4uHxuhXJc4m-9rEuNtSLjv4Hl0q5jIvhLFf-WZAJJFzd01W4_R3h5hxyt4pWbNt8FtowFjuM3QiW2MkXr2pQMvZPPxC1dn4xHyQ32T4uy2SPwrZRDGuFtUj1ttw46uX6tQc87EbkR1sPaTCdOVYoS5MAeYX9v1WfZSP1wME25f6Qoz4AOVoAVN16Q34CttsBxiO6u3NTce88xXNbZEW-7rD9t7QebRbWONatAQioAN0sTzu-KMVmwl9lMv2WgUsGkgQLvmzI9-YzjOXYZzzr_Iwrrb3SNsDuxcsEYbtzKWYlm2zYSy4TV38jVx6RPYJUvAAZzkzPMSdErS0_AAj2l96hGg-ibC2Uqs5q9YrqBdLDilxsOEjHmKduaQr6o-L7B4i1DftPrLQABoxZ9nzH0GygzchrYXi8KfMyorvLofQGql4sT7OR9s7su4RMY4wSanuejUhho2Zl_pwFstoBWT5lXf2Hco_c2XIU9QFWXF2qD_U38K1m36FHaQyED_n9l1fIkcOHni6-DiTHCZiuGkP0K1c_wcP7SOypnlzXG9opsB1un_13HeO52xILj1gs4OBAWVk6-cMhO73kBXV-J_4We3wrjERd7wdAMY-my7_OF52h8-iwKAZo_ARX_yi6GGC6gp2PaH2A3AKGlKuj1vz1i9rHfnqUX74bVCt4xJZG5kWVdsv8oSeLcnptRoOqWScZFNIykgpLgsSXVGIJ172Hg9PmrJflPh1SDOiLzjUZs5SDTqMjInnj5_dGpVMONaz0m5qXt8HPc_Xx4E_X5JZW3oHwCJaOg3oKkQAHq3viyDzhb_8Ek182jbE2kpK8Uz7DIxwDAA7oV1XVwQuH_Uyg6OjO-TbBDbEIRhgUa9HDosc1GfP4apCwVnFLYgclcLGAIunRKdC9JPUhTb_Sie5CTxEj30ZLYQsF0JiwgROzlQnPAA7sRF-l5Jk6BvHR1l5XmisHKNRRfT_voBcyRzbiY2EXo0gYynTq8xurXqRXk2wi5u_LQMWGlw5Le7NcgBCvDxl1Ht5S_MW3z50AJI3vuBUamDPc6FlTGSNfo23li5V2RwPLYHhIDlFvw_rEras808QdEfRPUW0QYxhUBSx1QbnVP2BB0RmCI2kRcthMqKEb-rTs8EgvNEUG__yogmZQ6NGgGI4PnZsjMajP9k0UfSd1T8GJzGPaGCzMdEjCa5G3I3Te2oZECBvNrIAVQHRyZAyo4cgYhogvBpKHIyU9Wu7qDmZxuy';
+var token = '7RnXXNLgkix3uZWkOoe48T0t_Jz5tECahcIsSHB_dK1ZoXauQEsSdQ_qvgLKu2osJRavy3QIz7t3J9p1OGJ4BMpGDmEGNeHZVwmbfK6pcfqru40uxBDATAhy_J5LF2vJ0l1Ugx_53_wYcKv1Uiz9ldiPjMtgA7C7oso81P9yHX4oepgy9bqHsCyBJYn2_lmF-IkZJCZeFJ3lWvSrYKJSACcu7OpwYd_jIZFptiRuTmnscFNMWvXOYuvUWFD9kq4EW9I1VTVi29rlNianLQHL5v63OK7pf0qqHVKSsOHX9raXql2aZg2ygHGhfGq4YJlQa3ffxZZrfd9ygiD3Y0W1PrL4gEASF8LHFjiJUsW83E4Erx23asLnBweZlR--Qo3NnWLgqNi5_V_fkQ2kYcLkqECjHcmyO-Aml-CrCSj3jNbmTQoAUQxboCbLYLnk2NEEPIUJnwE89Uf81YH7nsjkUopLYk78BpCw3wX1G8OAFXp6doP7oUoEScwBgvZ2XCQu2JHtLZSyK8fAIlp_BW8NfPCPeUPVZVrsNSvmaMT-wzoLlD4LxeO4JotVs9XUFVyooOceu-WMK9Oomx7fVmAJW7ux5gzazv-TJRn97Ya0eUQemgvFTt39GIUXaGNN-t6XhV8VgqTwZHX9Mj2O29KdbyUZfA0V3JJJugDNDsmvzHMsEw_dlXP1V-d-CEbjJu5VUST6-aDmwSsbygUDrJPw2g_ejD7Po917-0n6xniT8KI8YN5cQuWMZmSgolRC66doY6HEczWBps9g9neSu089_aXm_9VibMoRDgVTuNwwByDjClFZgtF52JSZAFKkSNG46UWWXdJZ4wY5fUxUlkiK4q-iWK92decsJrSChK7YrLc4zBRXL3iyLlP-unWmqhX8r9-7ceBO8-iiL-NPej61zvH0XFy84EbVnbPGfZpC4d0Sh1Q0uirKfFu-m4VulVr9ds5sHk6mF4dOIJ2ArfyBPAX28frJYADLgf4RVAxLRdrz2ep97vm2Ky3viKdmvHkBCW_z6ZQ_Od5JccJGAmeS3zK5MyNJTUbKiCXJyw5lMmy5bjXpUlUGcSeltAwglO4u4rkGNLK0gD3VpukwKkDfKYZDPdq96QMrirPrcAXTtpFr1jycWUMNeHlEA1nd14PPlLd8M8vANcEhZPTxjaILWyH1USBByeaE8OjGozKASaQXHqZE-iQyTwHrz0_P28X3NxML3mQXio6MeCdUOfW2CkXVI2hnKVnU-k99uGbvi-GkNGXkmgB0W6lGcPqRhOpYJgPFLODHtNRuxrTsNkmun58w1e82P3lmBad7YapUK-fYnnHhD08VmnjsOUH73qtMFeIQfuDTg-HrYYfAE9gQ2fUYhuSwvlv9_X2-SjZ4XuPJKymnsf_wyPac-i9kQlfo0tvgwH8dyMSUhZeVvIdgig82mjGgAgSHDC-1_tUI5JCmdTwlagy71RrI8VEpy-k9WGM_m3eX82uyBxytQaMvCKQAN2aNQwvFGAzlCGlUTvEk1eh99c1hVetqiz04N5GNczHQ_tjWgcK69YzvRMzMm0i5wJ2LjUVFU9wioMuASYvt5uBizvtP5PBimBvLOnQm';
 
-var client = new elasticsearch.Client();
+var client = new elasticsearch.Client({
+  host: 'https://lo0l35z0:6mnvwo0j46ih3gym@fig-7632293.us-east-1.bonsaisearch.net'
+});
+
 client.indices.exists(
   {
     index: 'vision'
   },
   function (error, response, status) {
-    if (error) {
-      throw new Error();
-    }
+    if (error) throw error;
 
     if (!response) {
       client.indices.create(
@@ -48,9 +49,7 @@ client.indices.exists(
           }
         },
         function (error) {
-          if (error) {
-            throw new Error();
-          }
+          if (error) throw error;
 
           migrate();
         }
@@ -63,75 +62,90 @@ client.indices.exists(
 );
 
 function migrate() {
-  var vehicleId = '105342';
+  var vehicleId1 = '105342';
+  var vehicleId2 = '581a365f5d5f735698a021bf';
   var date = '2016-05-20';
 
   request.get(
     {
-      url: 'http://localhost:65027/api/Trips/GetTrips?VehicleId=' + vehicleId + '&SelectedDate=' + date,
+      url: 'http://visiontest.danlawinc.com/api/Trips/GetTrips?VehicleId=' + vehicleId1 + '&SelectedDate=' + date,
       auth: {
         bearer: token
       }
     },
     function (error, response, body) {
-      if (error) {
-        console.log(error);
+      if (error) throw error;
+
+      if (response.statusCode != 200) {
+        console.log(response);
         return;
       }
 
-      if (response.statusCode == 200) {
-        var trips = JSON.parse(body);
-        trips.forEach(function (trip) {
-          console.log(trip.trip.tripId);
-          request.get(
-            {
-              url: 'http://localhost:65027/api/Trips/GetTripJsonData?TripId=' + trip.trip.tripId,
-              auth: {
-                bearer: token
-              }
-            },
-            function (error, response, body) {
-              if (error) {
-                console.log(error);
-                return;
-              }
+      var trips = JSON.parse(body);
 
-              if (response.statusCode == 200) {
-                var data = JSON.parse(body);
-                data.forEach(function (element) {
-                  var body = Object.assign(
-                    {
-                      tripId: trip.trip.tripId.toString(),
-                      vehicleId: vehicleId,
-                      tripNumber: trip.trip.tripNumber.toString()
-                    },
-                    element
-                  );
-                  // console.log(body);
-                  client.index(
-                    {
-                      index: 'vision',
-                      type: 'message',
-                      body: body
-                    },
-                    function (error, response) {
-                      if (error) {
-                        console.error(error.response);
-                        throw new Error();
-                      }
+      function processTrip(trips, i) {
+        if (i == trips.length) {
+          console.log('done', vehicleId1, date);
+          return;
+        }
 
-                      console.log(response);
-                    }
-                  );
-                });
-              }
+        var trip = trips[i];
+
+        console.log(trip.trip.tripId);
+        request.get(
+          {
+            url: 'http://visiontest.danlawinc.com/api/Trips/GetTripJsonData?TripId=' + trip.trip.tripId,
+            auth: {
+              bearer: token
             }
-          );
-        });
+          },
+          function (error, response, body) {
+            if (error) throw error;
+
+            if (response.statusCode == 200) {
+              var data = JSON.parse(body);
+
+              function processElement(data, j) {
+                if (j == data.length) {
+                  console.log('done', trip.trip.tripId);
+                  processTrip(trips, i + 1);
+                  return;
+                }
+
+                var element = data[j];
+
+                var body = Object.assign(
+                  {
+                    tripId: trip.trip.tripId.toString(),
+                    vehicleId: vehicleId2,
+                    tripNumber: trip.trip.tripNumber.toString()
+                  },
+                  element
+                );
+                // console.log(body);
+                client.index(
+                  {
+                    index: 'vision',
+                    type: 'message',
+                    body: body
+                  },
+                  function (error, response) {
+                    if (error) throw error;
+
+                    console.log(j);
+
+                    processElement(data, j + 1);
+                  }
+                );
+              }
+
+              processElement(data, 0);
+            }
+          }
+        );
       }
-      else {
-        console.log(response);
-      }
+
+      processTrip(trips, 0);
     }
   );
 }
